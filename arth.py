e0a46b4d29dc5724432e81825be227db1581d163
Mantri Sangeetha <167602556+sangeetha302@users.noreply.github.com>
2025-03-29 13:08:24 +0530

    Add files via upload

---
+++ arth.py
@@ -0,0 +1,16 @@
+import sys
+
+n1=int(sys.argv[1])
+n2=int(sys.argv[2])
+
+r1=n1+n2;
+r2=n1-n2;
+r3=n1/n2;
+r4=n1*n2;
+
+print("addition",r1);
+print("subtraction",r2);
+print("multiplication",r3);
+print("division",r4);
+
+
